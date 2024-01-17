c94f241506a337ca4787f485aed4bde2b7291ca5
JoakBouy <b.joak@alustudent.com>
2024-01-17 09:25:37 +0300

    fix🔧

--- math/linear_algebra/102-squashed_like_sardines.py
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -6,35 +6,36 @@ Module that concatenates two matrices
 
 def matrix_shape(matrix):
     """
-    Needs a matrix as input
-    Returns the shape as a list of integers
+        Get the matrix shape
     """
-    if type(matrix[0]) is not list:
-        return [len(matrix)]
-    else:
-        return [len(matrix)] + matrix_shape(matrix[0])
-
-
-def concat_recursive(mat1, mat2, axe):
-    """
-    Needs a matrix as input
-    Returns a concatenated matrix
-    """
-    result = []
-    if axe == 0:
-        result = mat1 + mat2
-        return result
-    for i in range(len(mat1)):
-        result.append(concat_recursive(mat1[i], mat2[i], axe - 1))
-    return result
+    matrix_shape = []
+    while (type(matrix) is list):
+        matrix_shape.append(len(matrix))
+        matrix = matrix[0]
+    return matrix_shape
 
 
 def cat_matrices(mat1, mat2, axis=0):
     """
-    Needs a matrix as input
-    Returns the resulting matrix
+        concatenate a matrix
     """
-    if len(matrix_shape(mat1)) > axis and len(matrix_shape(mat2)) > axis:
-        return concat_recursive(mat1, mat2, axis)
-    else:
+    from copy import deepcopy
+    shape1 = matrix_shape(mat1)
+    shape2 = matrix_shape(mat2)
+    if len(shape1) != len(shape2):
         return None
+    for i in range(len(shape1)):
+        if i != axis:
+            if shape1[i] != shape2[i]:
+                return None
+    return rec(deepcopy(mat1), deepcopy(mat2), axis, 0)
+
+
+def rec(m1, m2, axis=0, current=0):
+    """
+        Do some reclusive calling
+    """
+    if axis != current:
+        return [rec(m1[i], m2[i], axis, current + 1) for i in range(len(m1))]
+    m1.extend(m2)
+    return m1
