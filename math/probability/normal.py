2fb35bed1907fa3672360bb66967117f4dd5e6db
JoakBouy <b.joak@alustudent.com>
2024-02-06 19:34:00 +0300

    Implemented the Normal class constructor to handle the initialization of the class instance - The constructor takes three parameters: data, mean, and stddev

---
+++ math/probability/normal.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Representing normal distribution
+"""
+
+
+class Normal:
+    """
+    Class normal distribution
+    """
+    e = 2.7182818285
+    pi = 3.1415926536
+
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """
+        Class contructor
+        """
+        if data is None:
+            if stddev <= 0:
+                raise ValueError("stddev must be a positive value")
+            else:
+                self.mean = float(mean)
+                self.stddev = float(stddev)
+        else:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.mean = sum(data) / len(data)
+            self.stddev = sum(map(lambda i: (i - self.mean) ** 2, data))
+            self.stddev = (self.stddev / len(data)) ** (1 / 2)
