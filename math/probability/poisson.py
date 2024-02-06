83d4b218a0f588c413e4bf3dc5e987ecc4ab4a21
JoakBouy <b.joak@alustudent.com>
2024-02-06 19:04:57 +0300

    Add cdf method to Poisson class
    - Implemented the cdf method in the Poisson class to calculate the cumulative distribution function for a given number of successes.
    - The cdf method handles non-integer inputs by converting them to integers and returns 0 for out-of-range inputs.

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,46 +1,56 @@
 #!/usr/bin/env python3
-
-class Poisson:
-    """
-    Represents a Poisson distribution.
-    
-    Attributes:
-        lambtha: The expected number of occurrences in a given time frame.
-    """
+"""
+Representing poisson distribution
+"""
 
 
 class Poisson:
+    """
+    Class poisson distribution
+    """
+    e = 2.7182818285
+
     def __init__(self, data=None, lambtha=1.):
+        """
+        Class contructor
+        """
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
             else:
                 self.lambtha = float(lambtha)
         else:
-            if not isinstance(data, list):
+            if type(data) is not list:
                 raise TypeError("data must be a list")
-            elif len(data) < 2:
+            if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            else:
-                self.lambtha = float(sum(data) / len(data))
-
-    def factorial(self, n):
-        fact = 1
-        for i in range(1, n + 1):
-            fact *= i
-        return fact
-
-    def exp(self, x):
-        n, term, sum = 0, 1, 1
-        while abs(term) > 1e-10:
-            n += 1
-            term *= x / n
-            sum += term
-        return sum
+            self.lambtha = sum(data) / len(data)
 
     def pmf(self, k):
+        """
+        Probability Mass Function for poisson
+        """
+        if type(k) is not int:
+            k = int(k)
         if k < 0:
             return 0
-        k = int(k)
-        e = self.exp(-self.lambtha)
-        return e * self.lambtha**k / self.factorial(k)
+        e_mean = Poisson.e ** - self.lambtha
+        mean_k = self.lambtha ** k
+        x_factorial = 1
+        for i in range(1, k + 1):
+            x_factorial *= i
+        pmf = e_mean * mean_k / x_factorial
+        return pmf
+
+    def cdf(self, k):
+        """
+        Cumulative Distribution Function for poisson
+        """
+        if type(k) is not int:
+            k = int(k)
+        if k < 0:
+            return 0
+        cdf = 0
+        for i in range(k + 1):
+            cdf += Poisson.pmf(self, i)
+        return cdf
