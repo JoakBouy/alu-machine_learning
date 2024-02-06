1d72df8398aa917d4e1dd4d948b70c9e4e1f04fe
JoakBouy <b.joak@alustudent.com>
2024-02-06 19:40:54 +0300

    Added the cdf method to the Normal class
    - The cdf method calculates the Cumulative Distribution Function (CDF) for a given x-value x

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -50,3 +50,13 @@ class Normal:
         coeficient = 1 / (self.stddev * (2 * Normal.pi) ** (1 / 2))
         pdf = coeficient * Normal.e ** exponent
         return pdf
+
+    def cdf(self, x):
+        """
+        Cumulative Distribution Function for normal
+        """
+        val = (x - self.mean) / (self.stddev * (2 ** (1 / 2)))
+        erf1 = (2 / Normal.pi ** (1 / 2))
+        erf2 = (val - (val**3)/3 + (val**5)/10 - (val**7)/42 + (val**9)/216)
+        cdf = (1 / 2) * (1 + erf1 * erf2)
+        return cdf
