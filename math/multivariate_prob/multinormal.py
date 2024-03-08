f345dd696eb3f7f42925744629358ba4f69302a7
JoakBouy <b.joak@alustudent.com>
2024-03-08 17:36:02 +0300

    Update: Add def pdf(self, x): that calculates the PDF at a data point

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -29,4 +29,25 @@ class MultiNormal:
         X_mean = data - self.mean
         self.cov = (X_mean @ X_mean.T) / (n - 1)
 
+    def pdf(self, x):
+        """
+        calculates the PDF
+        :param x: numpy.ndarray of shape (d, 1) containing the data point whose
+        PDF should be calculated
+            d is the number of dimensions of the Multinomial instance
+        :return: the value of the PDF
+        """
+        if type(x) is not np.ndarray:
+            raise TypeError("x must be a numpy.ndarray")
+        d = self.cov.shape[0]
+        if len(x.shape) != 2 or x.shape[1] != 1 or x.shape[0] != d:
+            raise ValueError("x must have the shape ({}, 1)".format(d))
 
+        det = np.linalg.det(self.cov)
+        inv = np.linalg.inv(self.cov)
+        first = 1 / ((2 * np.pi) ** (d / 2) * np.sqrt(det))
+        second = np.dot((x - self.mean).T, inv)
+        third = np.dot(second, (x - self.mean) / -2)
+        pdf = first * np.exp(third)
+
+        return pdf[0][0]
