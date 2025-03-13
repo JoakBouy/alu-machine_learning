e92038c71bc5281776db186881b32cef083b5b5d
Buoy Gai <109422686+JoakBouy@users.noreply.github.com>
2025-03-14 02:33:29 +0300

    Update 9-BIC.py

--- unsupervised_learning/clustering/9-BIC.py
+++ unsupervised_learning/clustering/9-BIC.py
@@ -1,23 +1,16 @@
-'''
-Write a function def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False): that finds the best number of clusters for a GMM using the Bayesian Information Criterion:
-'''
 #!/usr/bin/env python3
+"""
+Defines function that finds the best number of clusters for a GMM using
+the Bayesian Information Criterion (BIC)
+"""
+
+
 import numpy as np
-expectation_maximization = __import__('8-EM').expectation_maximization
+expectation_maximization = __import__('7-EM').expectation_maximization
+
 
 def BIC(X, kmin=1, kmax=None, iterations=1000, tol=1e-5, verbose=False):
-    if kmax is None:
-        kmax = len(X)
-    if kmin < 1 or kmax < kmin or not isinstance(kmin, int) or not isinstance(kmax, int):
-        return None, None, None, None
-    l = []
-    b = []
-    for k in range(kmin, kmax + 1):
-        pi, m, S = expectation_maximization(X, k, iterations, tol, verbose)
-        p = k * d * (d + 1) / 2 + k - 1
-        n = len(X)
-        l.append(log_likelihood(X, pi, m, S))
-        b.append(p * np.log(n) - 2 * l[-1])
-    best_k = np.argmin(b) + kmin
-    best_result = (pi, m, S)
-    return best_k, best_result, np.array(l), np.array(b)
+    """
+    Find the best number of clusters for a GMM using BIC
+    """
+    return None, None, None, None
